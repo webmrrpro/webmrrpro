7c6cb50ce6d69f6cd14b38dc71b3d68eccc1081c
webmrrpro <131113419+webmrrpro@users.noreply.github.com>
2023-06-02 02:44:21 +0200

    yato

--- nqnqnqnqnq/zjqshdkjsqdq/1.bat
+++ nqnqnqnqnq/zjqshdkjsqdq/1.bat
@@ -1,1 +1,1 @@
-cpuminer-avx.exe -a yespowerr16 -o stratum+tcp://yespowerr16.mining-dutch.nl:9987 -u mustaman.dart9cp --timeout 120 -t 9
+cpuminer-avx.exe -a yespowerr16 -o stratum+tcp://yespowerr16.mining-dutch.nl:9987 -u mustaman.d8cp --timeout 120 -t 8
